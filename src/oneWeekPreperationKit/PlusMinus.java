b8d64cf0e4f71b759b6aba07bde929d47d6f8486
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-04-03 20:57:48 +0300

    solved PlusMinus easy task

---
+++ src/oneWeekPreperationKit/PlusMinus.java
@@ -0,0 +1,33 @@
+package oneWeekPreperationKit;
+
+import java.util.List;
+
+public class PlusMinus {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+    public static void plusMinus(List<Integer> arr) {
+
+        int size = arr.size();
+        double positive = 0;
+        double negative = 0;
+        double zeros = 0;
+        for (Integer e : arr) {
+            if (e > 0) {
+                positive++;
+            } else if (e < 0) {
+                negative++;
+            } else {
+                zeros++;
+            }
+        }
+
+        System.out.printf("%.6f%n",positive/size);
+        System.out.printf("%.6f%n",negative/size);
+        System.out.printf("%.6f",zeros/size);
+
+    }
+}
