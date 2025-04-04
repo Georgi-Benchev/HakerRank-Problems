e1d0e70a2bef424f0cadd303c00124790d8a1f14
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-04-04 21:02:34 +0300

    solved FindTheMedian with insertion sort

---
+++ src/oneWeekPreperationKit/FindTheMedian_InsertionSort.java
@@ -0,0 +1,27 @@
+package oneWeekPreperationKit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindTheMedian_InsertionSort {
+    public static int findMedian(List<Integer> arr) {
+        // Write your code here
+
+        List<Integer> mutableArr = new ArrayList<>(arr);
+
+        for (int i = 1; i < arr.size(); i++) {
+            int currentNum = mutableArr.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && mutableArr.get(j) > currentNum){
+                mutableArr.set(j+1, mutableArr.get(j));
+                j--;
+            }
+
+            mutableArr.set(j+1,currentNum);
+        }
+
+        return mutableArr.get(mutableArr.size()/2);
+
+    }
+}
