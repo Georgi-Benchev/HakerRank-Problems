41f4c1e37b8a02fee3ccea317cd21f8c570385e4
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-04-05 22:38:40 +0300

    solved towerBreakers

---
+++ src/oneWeekPreperationKit/TowerBreakers.java
@@ -0,0 +1,16 @@
+package oneWeekPreperationKit;
+
+public class TowerBreakers {
+    public static int towerBreakers(int n, int m) {
+
+        if (m == 1) {
+            return 2;
+        }
+        if (n % 2 == 0) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+}
